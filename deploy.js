2803ea9bd882ea0a76b5c3dfd7adcadaadd49ce7
Do Xuan Long <longdo@dobtech.vn>
2022-11-12 00:40:20 +0700

    use environment variable

--- deploy.js
+++ deploy.js
@@ -1,15 +1,11 @@
 const ethers = require('ethers');
 const fs = require('fs');
+require('dotenv').config();
 
 async function main() {
   // 127.0.0.1:8545
-  const provider = new ethers.providers.JsonRpcProvider(
-    'http://127.0.0.1:8545'
-  );
-  const wallet = new ethers.Wallet(
-    '0xf3d5da85ddc1ff714247fac44d9041f5509cddef5543ae349d1b1b6bdf86627d',
-    provider
-  );
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf8');
   const binary = fs.readFileSync(
     './SimpleStorage_sol_SimpleStorage.bin',
